7e412af6ad4701f6a9d8d0179489e8b4cef5b8d7
zn294 <9560020+zn294@user.noreply.gitee.com>
2022-03-01 17:35:41 +0800

    整理代码

--- src/main.js
+++ src/main.js
@@ -1,53 +1,48 @@
 import Vue from "vue";
-import App from "./App.vue";
 import router from "./router";
-import BaiduMap from "vue-baidu-map";
 import "./plugins/element.js";
-
+import App from "./App.vue"
 // 导入字体图标
 import "./assets/fonts/icon/iconfont.css";
 // 导入全局样式表
 import "./assets/css/global.css";
 
-// import echarts from 'echarts'
+
+
+// ----------------------------------------------------Echarts配置-----------------------------------------------
 import * as echarts from "echarts";
-
-import axios from "axios";
-
-import CommonFunction from "./common.js"
-Vue.prototype.CommonFunction=CommonFunction
-
 Vue.prototype.$echarts = echarts;
-Vue.prototype.$http = axios;
 
+
+
+// ---------------------------------------------------axios配置----------------------------------------------------------
+import axios from "axios";
+Vue.prototype.$http = axios;
 // 配置请求的根路径
-// axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.defaults.baseURL = "http://10.15.1.192:8000/apis";
-// 在 request 拦截器中，展示进度条 NProgress.start()
 axios.interceptors.request.use((config) => {
-  // console.log(config)
-  // NProgress.start()
   // 将token保存到authorization中
   config.headers.Authorization = window.sessionStorage.getItem("token");
   // 在最后必须 return config
   return config;
 });
-// 在 response 拦截器中，隐藏进度条 NProgress.done()
-// axios.interceptors.response.use(config => {
-//   NProgress.done()
-//   return config
-// })
-
-Vue.config.productionTip = false;
-// Vue.component('tree-table', TreeTable)
-// 将富文本编辑器，注册为全局可用的组件
-// Vue.use(VueQuillEditor)
 
 
+
+// ---------------------------------------------------公共方法配置--------------------------------------------------------------
+import CommonFunction from "./common.js"
+Vue.prototype.CommonFunction=CommonFunction
+
+
+
+// ----------------------------------------------------------百度地图配置------------------------------------------------------------
+import BaiduMap from "vue-baidu-map";
 Vue.use(BaiduMap, {
   ak: "HcOsUXOCYNGGuuayQnGXK7EG38pEglSA",
 });
 
+
+// -----------------------------------------------------注册实例---------------------------------------------
 new Vue({
   router,
   render: (h) => h(App),
