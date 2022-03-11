f03b15c06342e2d3c9e1f34ab2b22bec864ae904
zn294 <9560020+zn294@user.noreply.gitee.com>
2022-03-11 15:01:53 +0800

    1

---
+++ src/components/WholeMap/MapHome.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-row>
+    <el-col class="left"> </el-col>
+    <el-col class="mid">
+    
+    </el-col>
+    <el-col class="right"> </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  },
+  created() {},
+  methods: {
+
+  },
+};
+</script>
+
+<style lang="less" scoped></style>
