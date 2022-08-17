3b041be544f97476152bb6200457235a959b3595
Administrator <xx>
2022-08-17 09:37:55 +0800

    配置mock拦截api

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -3,7 +3,7 @@ const Mock = require('mockjs')   //安装的mockjs，并不是创建的mock.js
 // 获取 mock.Random 对象
 const Random = Mock.Random;
 //使用mockjs模拟数据
-Mock.mock('/api/data', (req, res) => {//当post或get请求到/api/data路由时Mock会拦截请求并返回上面的数据
+Mock.mock('http://10.15.1.192:8000/apis', (req, res) => {//当post或get请求到/api/data路由时Mock会拦截请求并返回上面的数据
     let list = [];
     for(let i = 0; i < 30; i++) {
         let listObject = {
