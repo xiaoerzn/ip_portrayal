9bdaff7831e24cde5514079aa6e7e973e229468f
Administrator <xx>
2022-09-01 16:13:30 +0800

    1111111111

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -1,20 +1,20 @@
-//引入mockjs
-const Mock = require('mockjs')   //安装的mockjs，并不是创建的mock.js
-// 获取 mock.Random 对象
-const Random = Mock.Random;
-//使用mockjs模拟数据
-Mock.mock('http://10.15.1.192:8000/apis', (req, res) => {//当post或get请求到/api/data路由时Mock会拦截请求并返回上面的数据
-    let list = [];
-    for(let i = 0; i < 30; i++) {
-        let listObject = {
-            title: Random.csentence(5, 10),//随机生成一段中文文本。
-            company: Random.csentence(5, 10),
-            attention_degree: Random.integer(100, 9999),//返回一个随机的整数。
-            photo: Random.image('114x83', '#00405d', '#FFF', 'Mock.js')
-        }
-        list.push(listObject);
-    }
-    return {
-        data: list
-    }
-})
+// //引入mockjs
+// const Mock = require('mockjs')   //安装的mockjs，并不是创建的mock.js
+// // 获取 mock.Random 对象
+// const Random = Mock.Random;
+// //使用mockjs模拟数据
+// Mock.mock('http://10.15.1.192:8000/apis', (req, res) => {//当post或get请求到/api/data路由时Mock会拦截请求并返回上面的数据
+//     let list = [];
+//     for(let i = 0; i < 30; i++) {
+//         let listObject = {
+//             title: Random.csentence(5, 10),//随机生成一段中文文本。
+//             company: Random.csentence(5, 10),
+//             attention_degree: Random.integer(100, 9999),//返回一个随机的整数。
+//             photo: Random.image('114x83', '#00405d', '#FFF', 'Mock.js')
+//         }
+//         list.push(listObject);
+//     }
+//     return {
+//         data: list
+//     }
+// })
